4e7432b6328525c38b815285f05cec70c5e1f667
alyssahuang <go2alyssa@gmail.com>
2018-07-25 13:44:52 -0400

    columns automated

--- :seqr:code:seqr:xbrowse_server:api:views.py
+++ :seqr:code:seqr:xbrowse_server:api:views.py
@@ -4,7 +4,7 @@
 def alyssa(request):
         cursor = connection.cursor()
         #cursor.execute("SELECT * from gc_1")
-        cursor.execute("SELECT sample, company, dbsnp_ins_del_ratio, num_singletons, mean_target_coverage, median_target_coverage, pct_target_bases_10x, pct_target_bases_20x, pct_target_bases_30x, pct_chimeras, contamination, het_homvar_ratio, total_snps, pct_dbsnp, dbsnp_titv, novel_titv, total_indels, pct_dbsnp_indels, novel_ins_del_ratio FROM gc_2 ORDER BY company")
+        cursor.execute("SELECT sample, company, dbsnp_ins_del_ratio, num_singletons, mean_target_coverage, median_target_coverage, pct_target_bases_10x, pct_target_bases_20x, pct_target_bases_30x, pct_chimeras, contamination, het_homvar_ratio, total_snps, pct_dbsnp, dbsnp_titv, novel_titv, total_indels, pct_dbsnp_indels, novel_ins_del_ratio, project_id, family_id FROM gc_2 ORDER BY company")
         columns = [col[0] for col in cursor.description]
         individual_rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
         individuals_json = []
@@ -21,15 +21,12 @@ def alyssa(request):
                 except:
                         col_json.append({'type': 'string'})
         row_json = []
+        trial_row = []
 
-        #ADDRESS
-        #hardcoded
         for indiv in individual_rows:
-                row_json.append({'c': [{'v': str(indiv["sample"])}, {'v': str(indiv["company"])}, {'v': float(indiv["dbsnp_ins_del_ratio"])}, {'v': float(indiv["num_singletons"])}, {'v': float(indiv["mean_target_coverage"])}, {'v': float(indiv["median_target_coverage"])}, {'v': float(indiv["pct_target_bases_10x"])}, {'v': float(indiv["pct_target_bases_20x"])}, {'v': float(indiv["pct_target_bases_30x"])}, {'v': float(indiv["pct_chimeras"])}, {'v': float(indiv["contamination"])}, {'v': float(indiv["het_homvar_ratio"])}, {'v': float(indiv["total_snps"])}, {'v': float(indiv["pct_dbsnp"])}, {'v': float(indiv["dbsnp_titv"])}, {'v': float(indiv["novel_titv"])}, {'v': float(indiv["total_indels"])}, {'v': float(indiv["pct_dbsnp_indels"])}, {'v': float(indiv["novel_ins_del_ratio"])}]});
-                #row_json.append({'c': [{'v': str(indiv["date"])},{'v': indiv["value"]}]});
-                #break
+                row_json.append({'c': [{'v': str(indiv[str(c)] )} for c in columns] });
 
-        final_json = {'cols': col_json, 'rows': row_json}
+        final_json = {'cols': col_json, 'rows': row_json, 'col_names': columns}
         #print(json.dumps(final_json))
 
         return JSONResponse(json.dumps(final_json))
